9fc079bd2b52b5c3114e4549ec76218317d4ff3e
Дмитрий Пуляев <dmitrijpulaev@MacBook-Air-dima.local>
2023-11-02 17:31:02 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const pictures = [
+  'images/fuji_large.png',
+  'images/street_large.png',
+  'images/forest_large.png'
+];
+
+
+const thumbnails = document.querySelectorAll('.gallery__picture-preview');
+const fullPicture = document.querySelector('.full-picture');
+
+const addThumbnailClickHandler = function(thumbnail, picture){
+  thumbnail.addEventListener('click', function () {
+    fullPicture.src = picture;
+  });
+};
+for (let i = 0; i < thumbnails.length; i++){
+  addThumbnailClickHandler(thumbnails[i], pictures[i]);
+}
+
+
+
+
